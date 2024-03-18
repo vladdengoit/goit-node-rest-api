12c56ca6d13b16d08169bf4c52f76fb6b46e706c
vladdengoit <114827862+vladdengoit@users.noreply.github.com>
2024-03-18 22:15:54 +0200

    hw2finishLine

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,83 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js"
+import { createContactSchema , updateContactSchema } from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res,next) => {
+ try {
+  const result = await contactsService.list()
+  res.json(result)
+  
+ }
+  catch (error) {
+   next(error)
+ }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res , next) => {
+  try {
+    const {id} = req.params;
+    const result = await contactsService.get(id)
+    
+    if (!result) {
+      throw HttpError(404, `Contact with id=${id} not found`);
+        }
+    res.json(result)
+   
+    
+   }
+    catch (error) {
+     next(error)
+   }
+  };
 
-export const deleteContact = (req, res) => {};
 
-export const createContact = (req, res) => {};
 
-export const updateContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const {id}= req.params
+    const result = await contactsService.remove(id)
+    if (!result) {
+      throw HttpError(404, `Contact with id=${id} not found`);
+        }
+        res.json({
+          message: "Delete success"
+      });
+  }
+   catch (error) {
+    next(error)
+  }
+};
+
+export const createContact = async (req, res, next) => {
+  try {
+    const {error} = createContactSchema.validate(req.body)
+    if(error) {
+      throw HttpError(400, error.message);
+  }
+    const result = await contactsService.add(req.body)
+    res.status(201).json(result)
+    
+  } catch (error) {
+    next(error)
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  try {
+    const {error} = updateContactSchema.validate(req.body)
+    if(error) {
+      throw HttpError(400, error.message);
+  }
+
+    const {id} = req.params;
+    const result = await contactsService.update(id, req.body)
+    if (!result) {
+      throw HttpError(404, `Contact with id=${id} not found`);
+        }
+    res.json(result)
+    
+  } catch (error) {
+    next(error)
+    
+  }
+};
---
+++ services/contactsServices.js
@@ -0,0 +1,59 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from 'nanoid'
+
+const contactsPath  = path.resolve("../goit-node-rest-api-1", "db","contacts.json")
+
+const updateAllContacts = contacts => fs.writeFile(contactsPath, JSON.stringify(contacts,null,2))
+
+export async function list() {
+  const data = await fs.readFile(contactsPath, "utf-8")
+  return JSON.parse(data)
+}
+
+export async function get(contactId) {
+  const listAllContacts = await list()
+  const contactsById = listAllContacts.find(el=> el.id === contactId)
+ 
+  return contactsById || null ;
+}
+
+export async function remove(contactId) {
+  const listAllContacts = await list()
+  const indexforDelete = listAllContacts.findIndex( el => el.id===contactId)
+  
+if(indexforDelete === -1){
+  return null;
+}
+const [result] = listAllContacts.splice(indexforDelete, 1)
+await fs.writeFile(contactsPath, JSON.stringify(listAllContacts , null , 1));
+return result
+  
+}
+
+ export async function add(data) {
+  const listAllContacts = await list()
+  const newContact = {
+    id:nanoid(),
+    ...data
+  };
+  listAllContacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(listAllContacts , null , 1));
+  return newContact;
+}
+
+
+export async function update(id, data) {
+ 
+  const listAllContacts = await list()
+  const indexforUpdate = listAllContacts.findIndex( el => el.id===id)
+ 
+if(indexforUpdate === -1){
+  return null;
+}
+listAllContacts[indexforUpdate]={...listAllContacts[indexforUpdate],...data}
+await updateAllContacts(listAllContacts)
+
+return listAllContacts[indexforUpdate]
+  
+}
